7d28d0194b546e978d646dd14056705563cbf5fe
makoto1975@gmail.com <makoto1975@gmail.com@c0cfd5a1-c1d0-4010-10ba-c253f495be87>
2011-02-07 04:34:58 +0000

    - felica を扱う共通機能を nfc-felica-lib に移動しました。

--- nfc-felica/trunk/nfc-felica-lib/src/net/kazzz/felica/IFeliCaByteData.java
+++ nfc-felica/trunk/nfc-felica-lib/src/net/kazzz/felica/IFeliCaByteData.java
@@ -9,7 +9,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.kazzz.felica.lib;
+package net.kazzz.felica;
 
 /**
  * FeliCaで使用するバイトデータを抽象化したインタフェース
